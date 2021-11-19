66ecf1af975af2fa8f7b98d90e9d3427c4542dbf
ArGoN <70173258+ArGoNArGoN@users.noreply.github.com>
2021-11-19 22:45:08 +0300

    Добавлены папки: MVVM, Service. Создан базовый класс для команд и интерфейс для сохранения данных в файл

---
+++ complex-task/ClientManager/Service/ISaveData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManager.Service
+{
+    public interface ISaveData<T>
+    {
+        void SaveData(T ob);
+        void DeleteData(T ob);
+
+        T GetData();
+    }
+}
